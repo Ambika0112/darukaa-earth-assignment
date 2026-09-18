e6a1d6052643cf8d8d7d7cb80d94151be32adffe
Ambika0112 <ambikamalgaya2@gmail.com>
2026-09-18 14:03:14 +0530

    Fix production CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,8 @@ app = FastAPI(title="Darukaa Earth API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173"
+        "https://darukaa-earth-assignment-bay.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
